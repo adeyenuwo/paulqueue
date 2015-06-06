001a54c3f9e56250a73484fac792a0bc75813265
Paul Ade <adeyenuwo@gmail.com>
2015-06-06 12:35:56 -0400

    Initial Commit

---
+++ PaulQueue/PaulQueue.h
@@ -0,0 +1,13 @@
+//
+//  PaulQueue.h
+//  PaulQueue
+//
+//  Created by Paul Ade on 2015-06-06.
+//  Copyright (c) 2015 Paul Ade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PaulQueue : NSObject
+
+@end
---
+++ PaulQueue/PaulQueue.m
@@ -0,0 +1,13 @@
+//
+//  PaulQueue.m
+//  PaulQueue
+//
+//  Created by Paul Ade on 2015-06-06.
+//  Copyright (c) 2015 Paul Ade. All rights reserved.
+//
+
+#import "PaulQueue.h"
+
+@implementation PaulQueue
+
+@end
