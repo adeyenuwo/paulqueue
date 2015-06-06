106aa1b1d80bb8df6535a80f17509c9a26c51cba
Paul Ade <adeyenuwo@gmail.com>
2015-06-06 13:04:05 -0400

    Initial commit of the working implementation of a Queue

--- PaulQueue/PaulQueue.h
+++ PaulQueue/PaulQueue.h
@@ -2,12 +2,21 @@
 //  PaulQueue.h
 //  PaulQueue
 //
-//  Created by Paul Ade on 2015-06-06.
-//  Copyright (c) 2015 Paul Ade. All rights reserved.
+//  Created by Paul Ade on 2014-01-08.
+//  Copyright (c) 2014 Paul Ade. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface PaulQueue : NSObject
+@interface PaulQueue : NSObject {
+    NSMutableArray *queue;
+}
+
+// Operatios supported by PaulQueue
+- (id)enqueue:(id)obj;
+- (id)dequeue;
+- (BOOL)isEmpty;
+- (NSUInteger)size;
+- (void)clear;
 
 @end
--- PaulQueue/PaulQueue.m
+++ PaulQueue/PaulQueue.m
@@ -2,12 +2,49 @@
 //  PaulQueue.m
 //  PaulQueue
 //
-//  Created by Paul Ade on 2015-06-06.
-//  Copyright (c) 2015 Paul Ade. All rights reserved.
+//  Created by Paul Ade on 2014-01-08.
+//  Copyright (c) 2014 Paul Ade. All rights reserved.
 //
 
 #import "PaulQueue.h"
 
 @implementation PaulQueue
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        queue = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (id)enqueue:(id)obj {
+    
+    [queue addObject:obj];
+    return queue;
+}
+
+- (id)dequeue
+{
+    id firstObject = nil;
+    firstObject = [queue objectAtIndex:0];
+    if (firstObject)
+        [queue removeObject:firstObject];
+    return firstObject;
+        
+}
+
+- (BOOL)isEmpty {
+    return [queue count] == 0;
+}
+
+- (NSUInteger)size {
+    return [queue count];
+}
+
+- (void)clear {
+    [queue removeAllObjects];
+}
 
 @end
